ee28f0ee15c4bc0d3ff7945b52fac9186009df13
lietryon <29160922+lietryon@users.noreply.github.com>
2020-10-07 16:07:52 +0200

    Initial commit

---
+++ Financier/Login_form.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Financier
+{
+    partial class Login_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.accNumber = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Account Number";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // accNumber
+            // 
+            this.accNumber.Location = new System.Drawing.Point(165, 138);
+            this.accNumber.Name = "accNumber";
+            this.accNumber.Size = new System.Drawing.Size(117, 20);
+            this.accNumber.TabIndex = 2;
+            this.accNumber.TextChanged += new System.EventHandler(this.accNumber_TextChanged);
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(165, 179);
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(117, 20);
+            this.password.TabIndex = 3;
+            this.password.TextChanged += new System.EventHandler(this.password_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(165, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Login_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 318);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.accNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login_form";
+            this.Text = "Login_form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox accNumber;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Financier/Login_form.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Financier
+{
+    public partial class Login_form : Form
+    {
+        public Login_form()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void accNumber_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Financier_dbEntities dbe = new Financier_dbEntities();
+            if (accNumber.Text != string.Empty || password.Text != string.Empty)
+            {
+                var user1 = dbe.Admin_Table.FirstOrDefault(a => a.AccountNumber.Equals(accNumber.Text));
+                if (user1 != null)
+                {
+                    if (user1.Password.Equals(password.Text))
+                    {
+                        Menu m1 = new Menu();
+                        m1.ShowDialog();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Incorrect password");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Incorrect values");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please, enter your Account Number and Password");
+            }
+        }
+    }
+}
